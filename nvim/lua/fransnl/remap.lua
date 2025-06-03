f588696b748b6929780e933356616f83f58b0b45
fransnl <nl.frans@gmail.com>
2025-06-03 14:00:55 +0200

    configs

---
+++ nvim/lua/fransnl/remap.lua
@@ -0,0 +1,8 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+print("fransnl")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Telescope find git files' })
+vim.keymap.set('v', '<leader>y', '"+y', opts)
